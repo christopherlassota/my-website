56b1ae5f8ad88f5f1150a130628065deec3f46d5
christopher <christopherlassota@outlook.com>
2025-06-15 21:45:22 -0600

    add featured projects content

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,13 @@
+import "./Home.scss";
 import FeaturedProjects from "../../Components/FeaturedProjects/FeaturedProjects"
 import Hero from "../../Components/Hero/Hero"
 
 const Home = () => {
     return (
-        <>
+        <section className="home">
         <Hero />
         <FeaturedProjects />
-        </>
+        </section>
     )
 }
 
