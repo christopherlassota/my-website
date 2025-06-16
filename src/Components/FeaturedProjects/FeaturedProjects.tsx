723f1db0c18a6593ffd33e97dbf41dd7e535e371
christopher <christopherlassota@outlook.com>
2025-06-16 10:20:59 -0600

    complete featuredprojects section

--- src/Components/FeaturedProjects/FeaturedProjects.tsx
+++ src/Components/FeaturedProjects/FeaturedProjects.tsx
@@ -3,37 +3,39 @@ import "./FeaturedProjects.scss";
 const FeaturedProjects = () => {
   return (
     <section className="featuredprojects">
-      <h1 className="featuredprojects__title">Featured Projects</h1>
-      <h2 className="featuredprojects__subtitle">
+      <h2 className="featuredprojects__title">Featured Projects</h2>
+      <h3 className="featuredprojects__subtitle">
         A selection of my projects that showcase my expertise and passion for
         development
-      </h2>
-      <article className="featuredprojects__card">
-        <div className="featuredprojects__image"></div>
-        <h3 className="featuredprojects__card-title">InStock</h3>
-        <p className="featuredprojects__card-description">
-          Bandsite was one of my first projects, I learned how to use flexbox to
-          great effect
-        </p>
-        <ul className="featuredprojects__taglist">
+      </h3>
+      <div className="featuredprojects__card-container">
+        <article className="featuredprojects__card">
+          <div className="featuredprojects__image"></div>
+          <h4 className="featuredprojects__card-title">InStock</h4>
+          <p className="featuredprojects__card-description">
+            Bandsite was one of my first projects, I learned how to use flexbox
+            to great effect
+          </p>
+          <ul className="featuredprojects__taglist">
             <li className="featuredprojects__tag">Javascript</li>
             <li className="featuredprojects__tag">HTML</li>
             <li className="featuredprojects__tag">CSS</li>
-        </ul>
-      </article>
-      <article className="featuredprojects__card">
-        <div className="featuredprojects__image"></div>
-        <h3 className="featuredprojects__card-title">InStock</h3>
-        <p className="featuredprojects__card-description">
-          Bandsite was one of my first projects, I learned how to use flexbox to
-          great effect
-        </p>
-        <ul className="featuredprojects__taglist">
+          </ul>
+        </article>
+        <article className="featuredprojects__card">
+          <div className="featuredprojects__image"></div>
+          <h4 className="featuredprojects__card-title">InStock</h4>
+          <p className="featuredprojects__card-description">
+            Bandsite was one of my first projects, I learned how to use flexbox
+            to great effect
+          </p>
+          <ul className="featuredprojects__taglist">
             <li className="featuredprojects__tag">Javascript</li>
             <li className="featuredprojects__tag">HTML</li>
             <li className="featuredprojects__tag">CSS</li>
-        </ul>
-      </article>
+          </ul>
+        </article>
+      </div>
     </section>
   );
 };
