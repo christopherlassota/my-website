34cc6df764b05af5fa4babc49bdb5492aebc2b00
christopher <christopherlassota@outlook.com>
2026-02-14 16:59:16 -0700

    chore: update agents doc and about content

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -9,6 +9,45 @@ const About = () => {
                 Delievering robust, scale-able, and user-focused solutions that drive buisness success
             </h3>
             < Experience />
+            <section className="education">
+                    <h2 className="education__title">
+                        Education
+                    </h2>
+                <article className="education__card">
+                    <div className="education__card-top">
+                        <p className="education__logo">
+                            MRU
+                        </p>
+                        <div className="education__information">
+                            <h3 className="education__degree">
+                                Bachelor's of Science In Cellular and Molecular Biology
+                            </h3>
+                            <h4 className="education__institute">
+                                Mount Royal University
+                            </h4>
+                            <p className="education__date">2018-2024</p>
+                        </div>
+                    </div>
+                    <p className="education__description">Earned a BSc in Cellular and Molecular Biology with a focus on genetics, biochemistry and lab reasearch</p>
+                </article>
+                <article className="education__card">
+                    <div className="education__card-top">
+                        <p className="education__logo">
+                            MRU
+                        </p>
+                        <div className="education__information">
+                            <h3 className="education__degree">
+                                Bachelor's of Science In Cellular and Molecular Biology
+                            </h3>
+                            <h4 className="education__institute">
+                                Mount Royal University
+                            </h4>
+                            <p className="education__date">2018-2024</p>
+                        </div>
+                    </div>
+                    <p className="education__description">Earned a BSc in Cellular and Molecular Biology with a focus on genetics, biochemistry and lab reasearch</p>
+                </article>
+            </section>
         </section>
     )
 }
